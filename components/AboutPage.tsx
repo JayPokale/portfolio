d7dd8023134c0c3aca3ce58f838c3dbf188cde0f
JayPokale <jay.pokale.35@gmail.com>
2022-10-22 13:01:12 +0530

    virgin

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { useEffect} from "react";
-import { Box} from "@chakra-ui/react";
 
+// Create new animation object similar to below object
 const mainVarients = {
   initial: {
     opacity: 0,
@@ -13,8 +13,6 @@ const mainVarients = {
     transition: { duration: 0.3, type: "spring", stiffness: 100 },
   },
 };
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const AboutPage: NextPage = () => {
   const [ref, inView] = useInView();
@@ -32,31 +30,14 @@ const AboutPage: NextPage = () => {
 
   return (
     <div className="max-w-7xl w-full mx-auto select-none">
-      <div ref={ref}>
+      <div ref={ref}> {/* Wtite content in this div */}
+
+        {/* Example of motion.div (Crete new animations similar to the mainVarients) */}
         <motion.div
           variants={mainVarients}
           animate={animation}
           initial="initial"
         >
-          <Box
-            scrollSnapType="x mandatory"
-            className="flex space-x-6 overflow-auto py-10 px-[calc(50%-160px)]"
-            css={{
-              "&::-webkit-scrollbar": { display: "none" },
-            }}
-          >
-            {arr.map((e) => {
-              return (
-                <Box
-                  key={e}
-                  className="h-80 w-80 max-w-full shrink-0 bg-white snap-center"
-                  style={{
-                    transform: "perspective(500px) rotateY(-30deg)",
-                  }}
-                ></Box>
-              );
-            })}
-          </Box>
         </motion.div>
       </div>
     </div>
